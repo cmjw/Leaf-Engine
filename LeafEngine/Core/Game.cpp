54adac933b22dfc4a7929c457acb308ebaa27354
Charlotte <cmjwfeltwell@gmail.com>
2024-12-08 18:58:40 -0600

    Game window

---
+++ LeafEngine/Core/Game.cpp
@@ -0,0 +1,70 @@
+#include "Core/Game.h"
+
+Game::Game() {
+	// default dimensions
+	gameWidth = 640;
+	gameHeight = 480;
+	zoom = 2;
+
+	this->Initialize();
+}
+
+Game::~Game() {
+	Cleanup();
+}
+
+/* Initialize game elements */
+void Game::Initialize() {
+	this->initGameWindow();
+}
+
+/* Initialize game window */
+void Game::initGameWindow() {
+	// create window
+	window.create(sf::VideoMode(gameWidth * zoom, gameHeight * zoom), "Engine");
+
+	view.setSize(gameWidth, gameHeight);
+	view.setCenter(gameWidth / 2, gameHeight / 2);
+
+	// set zoom
+	view.zoom(zoom / 2);
+	window.setView(view);
+}
+
+/* Run the game */
+void Game::Run() {
+	sf::Clock clock;
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		float deltaTime = clock.restart().asSeconds();
+
+		// update and render
+		this->Update(deltaTime);
+		this->Render();
+	}
+}
+
+/* Render the game window */
+void Game::Render() {
+	window.clear();
+
+	// render elements
+
+	window.display();
+}
+
+/* Update game elements */
+void Game::Update(float deltaTime) {
+	// update elements
+}
+
+/* Cleanup game elements */
+void Game::Cleanup() {
+	
+}
--- include/LeafEngine/Engine/Core/Game.h
+++ include/LeafEngine/Engine/Core/Game.h
@@ -4,4 +4,30 @@
 #include "SFML/Graphics.hpp"
 #include "ImGui/imgui.h"
 
+class Game {
+public:
+	Game();
+	~Game();
+
+	void Run();
+
+private:
+	int gameWidth;
+	int gameHeight;
+	float zoom;
+
+	std::string gameTitle;
+
+	sf::RenderWindow window;
+	sf::View view;
+
+	// core functions
+	void Initialize();
+	void Render();
+	void Update(float deltaTime);
+	void Cleanup();
+
+	void initGameWindow();
+};
+
 #endif // !GAME_H
