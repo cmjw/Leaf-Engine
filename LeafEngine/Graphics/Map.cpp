4bcfd4ecb77f47107b9e7a7866f4553e551ba44d
Charlotte <cmjwfeltwell@gmail.com>
2024-12-29 16:14:32 -0600

    Create Map.cpp

---
+++ LeafEngine/Graphics/Map.cpp
@@ -0,0 +1,21 @@
+#include "Graphics/Map.h"
+
+/* Map constructor */
+Map::Map() {
+
+}
+
+/* Map deconstructor */
+Map::~Map() {
+
+}
+
+/* Get the Map width */
+int Map::getWidth() {
+	return this->mapWidth;
+}
+
+/* Get the Map height */
+int Map::getHeight() {
+	return this->mapHeight;
+}
